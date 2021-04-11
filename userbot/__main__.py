826202e6e9cdbff328633717f719ab3dc78cb189
W29F <81409844+W29F@users.noreply.github.com>
2021-04-12 00:51:53 +0700

    TG-Lonely-WolfUbot

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"Lonelywolf-ubot ⚙️ V{BOT_VER} [☙ BERHASIL DIAKTIFKAN! ❧]")
+    f"TG-Lonely-WolfUbot ⚙️ V{BOT_VER} [☙ BERHASIL DIAKTIFKAN! ❧]")
 
 
 if len(argv) not in (1, 3, 4):
