b1d4b180b52640593db1f201c7f87b68d8377169
W29F <81409844+W29F@users.noreply.github.com>
2021-04-13 12:14:26 +0700

    TG-Lonely-WolfUbot

--- init/CI_Test_Script.sh
+++ init/CI_Test_Script.sh
@@ -13,7 +13,7 @@ export SEMAPHORE_PROJECT_DIR=`pwd`
 . "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/telegram
 TELEGRAM_TOKEN=${BOT_API_KEY}
 export BOT_API_KEY TELEGRAM_TOKEN
-tg_sendinfo "<code>I am gonna merge staging into Man-Userbot</code>"
+tg_sendinfo "<code>I am gonna merge staging into TG-Lonely-WolfUbot</code>"
 cd
 git clone https://github.com/mkaraniya/OpenUserBot.git
 cd OpenUserBot
