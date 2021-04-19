5f6ed3f64fdf074b8f339fa792cd58ed14dea127
W29F <81409844+W29F@users.noreply.github.com>
2021-04-19 19:27:14 +0700

    Update db.py

--- userbot/modules/db.py
+++ userbot/modules/db.py
@@ -18,13 +18,13 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern="^.db$")
 async def amireallydbs(dbs):
     if not is_mongo_alive() and not is_redis_alive():
-        db = "☙ Both Mongo and Redis Database seems to be failing! ❧"
+        db = "➳ Both Mongo and Redis Database seems to be failing!  "
     elif not is_mongo_alive():
-        db = "☙ Mongo DB seems to be failing! ❧"
+        db = "➳ Mongo DB seems to be failing!  "
     elif not is_redis_alive():
-        db = "☙ Redis Cache seems to be failing! ❧"
+        db = "➳ Redis Cache seems to be failing!  "
     else:
-        db = "☙ Databases functioning normally! ❧"
+        db = "➳ Databases functioning normally!  "
     await dbs.edit(
         ""
         f"**User:** `{DEFAULTUSER}` \n"
