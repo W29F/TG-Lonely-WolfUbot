316efa8ce0fcf2815aa50695d3ebb6ca6599f1e4
W29F <81409844+W29F@users.noreply.github.com>
2021-04-20 20:19:00 +0700

    Update lyrics.py

--- userbot/modules/lyrics.py
+++ userbot/modules/lyrics.py
@@ -18,27 +18,27 @@ if GENIUS is not None:
 
 @register(outgoing=True, pattern="^.lyrics (?:(now)|(.*) - (.*))")
 async def lyrics(lyric):
-    await lyric.edit("☙`Getting information...`❧")
+    await lyric.edit("➳`Getting information...` ")
     if GENIUS is None:
-        await lyric.edit("☙`Provide genius access token to Heroku ConfigVars...`❧")
+        await lyric.edit("➳`Provide genius access token to Heroku ConfigVars...` ")
         return False
     if lyric.pattern_match.group(1) == "now":
         playing = User(LASTFM_USERNAME, lastfm).get_now_playing()
         if playing is None:
-            await lyric.edit("☙`No information current lastfm scrobbling...`❧")
+            await lyric.edit("➳`No information current lastfm scrobbling...` ")
             return False
         artist = playing.get_artist()
         song = playing.get_title()
     else:
         artist = lyric.pattern_match.group(2)
         song = lyric.pattern_match.group(3)
-    await lyric.edit(f"☙`Searching lyrics for {artist} - {song}...`❧")
+    await lyric.edit(f"➳`Searching lyrics for {artist} - {song}...` ")
     songs = genius.search_song(song, artist)
     if songs is None:
-        await lyric.edit(f"☙`Song`  **{artist} - {song}**  `not found...`❧")
+        await lyric.edit(f"➳`Song`  **{artist} - {song}**  `not found...` ")
         return False
     if len(songs.lyrics) > 4096:
-        await lyric.edit("☙`Lyrics is too big, view the file to see it.`❧")
+        await lyric.edit("➳`Lyrics is too big, view the file to see it.` ")
         with open("lyrics.txt", "w+") as f:
             f.write(f"Search query: \n{artist} - {song}\n\n{songs.lyrics}")
         await lyric.client.send_file(
@@ -50,7 +50,7 @@ async def lyrics(lyric):
         return True
     else:
         await lyric.edit(
-            f"☙**Search query**:\n`{artist}` - `{song}`" f"\n\n```{songs.lyrics}```❧"
+            f"➳**Search query**:\n`{artist}` - `{song}`" f"\n\n```{songs.lyrics}``` "
         )
         return True
 
